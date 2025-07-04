97dd2ee661f2030998a26e52ed7e7bd637de5174
Myckhel <myckhel123@gmail.com>
2025-07-04 18:40:38 +0100

    chore: remove build artifacts and update main entry in package.json

--- build/index.modern.js
+++
@@ -1,7 +0,0 @@
-var _require = require('./utilities'),
-  Utilities = _require.Utilities;
-var _require2 = require('./coralpay-c-connect'),
-  CoralPayCConnect = _require2.CoralPayCConnect;
-module.exports.Utilities = Utilities;
-module.exports.CoralPayCConnect = CoralPayCConnect;
-//# sourceMappingURL=index.modern.js.map
